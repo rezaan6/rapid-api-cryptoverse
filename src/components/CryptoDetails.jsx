6a2464587b0c9edb01a312ea8e04af785fb966f2
rezaan6 <rezaan6@gmail.com>
2023-03-04 20:56:38 +0530

    24h Volume value fix

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -47,7 +47,7 @@ const CryptoDetails = () => {
     { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
     {
       title: '24h Volume',
-      value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+      value: `$ ${cryptoDetails?.['24hVolume'] && millify(cryptoDetails?.['24hVolume'])}`,
       icon: <ThunderboltOutlined />,
     },
     {
